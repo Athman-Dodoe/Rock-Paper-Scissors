eb547777e1ec8b1392290da572e74a35fb9db661
Athman-Dodoe <dodoeathman@gmail.com>
2023-08-28 12:48:15 +0300

    Added a playrpound function
    Plays one round of the game

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 function getComputerChoice() {
     let choices = ["rock","paper","scissor"]
     let choice = choices[Math.floor(Math.random() * choices.length)]
-    console.log(choice)
+    return choice;
  }
 
- getComputerChoice()
+ function playRound (playerSelection, computerSelection){  
+
+    if (playerSelection === computerSelection) {
+        return "Its a draw!"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissor") {
+        return("You win")
+    }
+
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return("You win")
+    }
+
+    else if (playerSelection === "scissor" && computerSelection === "paper") {
+        return("You win")
+    }
+
+    else {
+        return(`you loose ${computerSelection} beats ${playerSelection}`)
+    }
+
+
+ }
+
+const playerSelection = prompt("Enter your choice")
+const computerSelection = getComputerChoice();
+console.log("Player Selection:", playerSelection);
+console.log("Computer Selection:", computerSelection);
+console.log(playRound(playerSelection, computerSelection));
