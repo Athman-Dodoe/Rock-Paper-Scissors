de7af54a43d3a29957e795c11cfd2224d0ae3a8d
Athman-Dodoe <dodoeathman@gmail.com>
2023-08-28 10:41:19 +0300

    Created a getComputerChoice function
    shows the computer choice to the console

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello Odin");
+function getComputerChoice() {
+    let choices = ["rock","paper","scissor"]
+    let choice = choices[Math.floor(Math.random() * choices.length)]
+    console.log(choice)
+ }
+
+ getComputerChoice()
