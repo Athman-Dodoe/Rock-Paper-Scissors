b5779014519767b49cbfe2ee651008ee8fcb3299
Athman-Dodoe <dodoeathman@gmail.com>
2023-09-25 14:58:07 +0300

    Added EventListeners
    repsents the player Selection for each button when clicked

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+let rockBtn = document.getElementById('rock');
+let paperBtn = document.getElementById('paper');
+let scissorBtn = document.getElementById('scissor');
+
+rockBtn.addEventListener('click', function() {
+    const computerSelection = getComputerChoice();
+    let playerSelection = 'rock';
+    console.log("Player Selection:", playerSelection);
+    console.log("Computer Selection:", computerSelection);
+    let results = playRound(playerSelection, computerSelection);
+    console.log(results);
+    
+});
+
+
+paperBtn.addEventListener('click', function() {
+    const computerSelection = getComputerChoice();
+    let playerSelection = 'paper';
+    console.log("Player Selection:", playerSelection);
+    console.log("Computer Selection:", computerSelection);
+    let results = playRound(playerSelection, computerSelection);
+    console.log(results);
+});
+
+
+scissorBtn.addEventListener('click', function() {
+    const computerSelection = getComputerChoice();
+    let playerSelection = 'scissor';
+    console.log("Player Selection:", playerSelection);
+    console.log("Computer Selection:", computerSelection);
+    let results = playRound(playerSelection, computerSelection);
+    console.log(results);
+   
+});
+
+
+
 function getComputerChoice() {
     const choices = ["rock","paper","scissor"]
     let choice = choices[Math.floor(Math.random() * choices.length)]
@@ -25,49 +62,6 @@ function getComputerChoice() {
         return(`You Loose! ${computerSelection} beats ${playerSelection}`)
     }
 
-
  }
 
-let playerSelection;
 
-let countWins = 0;
-let countDraws = 0;
-let countLooses = 0;
- 
-for (let i = 0; i < 5; i++){
-    do {
-        playerSelection = prompt("Choose rock or paper or scissor").toLowerCase();
-    } while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissor');
-    
-    const computerSelection = getComputerChoice();
-    console.log("Player Selection:", playerSelection);
-    console.log("Computer Selection:", computerSelection);
-    let results = playRound(playerSelection, computerSelection);
-    console.log(results);
-
-    if (results === 'You win'){
-        countWins += 1;
-    }else if (results === 'Its a draw!'){
-        countDraws += 1;
-    
-    }else {
-        countLooses += 1;
-    }  
-}
-
-console.log('Game Results');
-console.log('----------------------------');
-
-console.log('No of wins:', countWins);
-console.log('No of Draws:', countDraws);
-console.log('No of Looses:', countLooses);
-
-console.log('----------------------------');
-
-if (countWins > countLooses) {
-    console.log('Player Selection Wins')
-}else if ( countWins < countLooses) {
-    console.log('Computer Selection Wins')
-}else {
-    console.log('Its a Draw');
-}
